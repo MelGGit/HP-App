864f32de5bf9b6bc2571fa53f994a37babe48a8a
Christof Jens <christofjens@gmail.com>
2021-05-07 16:56:34 +0200

    Add final changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('[data-js="characters"]')
-// const filterButtons = document.querySelectorAll('[data-js="filter"] > button')
+const filterButtons = document.querySelectorAll('[data-js="filter"] > button')
 
 let characters
 
 getAllCharacters().then(data => {
   characters = data
-  //  renderCharacters()
+  renderCharacters()
 })
 
 function getAllCharacters() {
@@ -37,34 +37,8 @@ function renderCharacter(character) {
 
   const el = document.createElement('div')
   el.className = 'character'
-  el.innerHTML = `
-  <section class="card" data-js="characters">
-  <div class="header-name">
-    <h2>${character.name}</h2>
-    House: ${character.house},<br>Species: ${character.species}
-  </div>
-  <div class="header-image">
-    <img class="card-image" width="150px" src="assets/favicon.png" alt="" />
-  </div>
-  <p>
-    ${character.name} ${character.alive ? '' : ' (deceased)'} is a
-    ${character.species} of House ${character.house}, portrayed by
-    ${character.actor}. ${character.gender === 'male' ? 'He' : 'She'} was born
-    on ${
-      character.dateOfBirth
-    } is currently/was a student/staff member of Hogwarts.
-    His/her patronus is ${character.patronus}.
-  </p>
-  <div class="wand-info">
-    <ul class="wand-list">
-      ${character.name}'s Wand
-      <li>Type of Wood: ${character.wand.wood}</li>
-      <li>Type of Core: ${character.wand.core}</li>
-      <li>Length: ${character.wand.length}</li>
-    </ul>
-  </div>
-</section>
-  `
+  // el.innerHTML = `
+
+  // `
   container.append(el)
 }
-console.log(characters)
