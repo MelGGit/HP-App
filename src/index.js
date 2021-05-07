f46b91bccb287ae6ec0bc1e008768c4aa06703c9
Christof Jens <christofjens@gmail.com>
2021-05-07 15:06:31 +0200

    Include innerHTML in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import AppHeader from './components/AppHeader'
-
-document.body.append(AppHeader('JavaScript App Template'))
-
 const container = document.querySelector('[data-js="characters"]')
 const filterButtons = document.querySelectorAll('[data-js="filter"] > button')
 
@@ -45,15 +41,37 @@ filterButtons.forEach(button => {
 // }
 
 function renderCharacter(character) {
-  const { name, image } = character
+  //  const { name, image } = character
 
   const el = document.createElement('div')
   el.className = 'character'
   el.innerHTML = `
-    <h2 class="character__name">${name}</h2>
-    <img 
-      class="character__image" 
-      src="${image.replace('http', 'https')}">
+  <section class="card" data-js="characters">
+  <div class="header-name">
+    <h2>${character.name}</h2>
+    House: ${character.house},<br>Species: ${character.species}
+  </div>
+  <div class="header-image">
+    <img class="card-image" width="150px" src="assets/favicon.png" alt="" />
+  </div>
+  <p>
+    ${character.name} ${character.alive ? '' : ' (deceased)'} is a
+    ${character.species} of House ${character.house}, portrayed by
+    ${character.actor}. ${character.gender === 'male' ? 'He' : 'She'} was born
+    on ${
+      character.dateOfBirth
+    } is currently/was a student/staff member of Hogwarts.
+    His/her patronus is ${character.patronus}.
+  </p>
+  <div class="wand-info">
+    <ul class="wand-list">
+      ${character.name}'s Wand
+      <li>Type of Wood: ${character.wand.wood}</li>
+      <li>Type of Core: ${character.wand.core}</li>
+      <li>Length: ${character.wand.length}</li>
+    </ul>
+  </div>
+</section>
   `
   container.append(el)
 }
