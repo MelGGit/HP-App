1c46a45317a05cbe36d7abeb588eb7ea54a2853e
Till Willing <tillwilling@gmail.com>
2021-05-10 10:14:29 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import setupButtons from './components/header/setupButtons'
 import setupHeader from './components/header/setupHeader'
+import setupSearch from './components/header/setupSearch'
 import renderCharacters from './renderCharacters.js'
 
 export let characters
@@ -16,34 +17,4 @@ function getAllCharacters() {
 
 setupHeader()
 setupButtons()
-
-// filterButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const selectedHouse = button.innerText
-//     renderCharacters(selectedHouse)
-//   })
-// })
-
-// function countByHouse(characters) {
-//   return characters.reduce((acc, cur) => {
-//     if (cur.house) {
-//       const houseCount = acc[cur.house]
-//       acc[cur.house] = houseCount ? houseCount + 1 : 1
-//     }
-//     return acc
-//   }, {})
-// }
-
-// function renderCharacter(character) {
-//   const { name, image } = character
-
-//   const el = document.createElement('div')
-//   el.className = 'character'
-//   el.innerHTML = `
-//     <h2 class="character__name">${name}</h2>
-//     <img
-//       class="character__image"
-//       src="${image.replace('http', 'https')}">
-//   `
-//   container.append(el)
-// }
+setupSearch()
